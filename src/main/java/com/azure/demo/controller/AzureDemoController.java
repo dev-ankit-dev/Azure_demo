23e4e9ac8eedfc78e0d449e24ebaa0ad0dfc9a33
ankit <ashumca108@gmail.com>
2021-08-21 00:18:06 +0530

    first commit in azure repo

---
+++ src/main/java/com/azure/demo/controller/AzureDemoController.java
@@ -0,0 +1,18 @@
+package com.azure.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureDemoController {
+	
+	@GetMapping
+	public String hello() {
+	
+		
+		return "Azure Pipeline";
+		
+		
+	}
+
+}
